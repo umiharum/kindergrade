c26576d888a47775daf664b560dda01d857b76df
umiharum <umiharum539@gmail.com>
2024-07-22 22:02:50 +0700

    changes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,12 +4,12 @@
             <h1 class="text-2xl font-semibold text-gray-900">{{ $slot }}</h1>
         </div>
         <div class="flex items-center">
-            <button type="button" class="relative text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+            {{-- <button type="button" class="relative text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                 <span class="sr-only">View notifications</span>
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                 </svg>
-            </button>
+            </button> --}}
             <div class="relative ml-3">
                 <button type="button" class="flex items-center max-w-xs bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500">
                     <span class="sr-only">Open user menu</span>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,5 +1,5 @@
 <a {{ $attributes }}
-    class="{{ $active ? 'text-white bg-gray-800' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} 
+    class="{{ $active ? 'text-gray-800 bg-gray-300' : 'text-gray-800 hover:bg-gray-700 hover:text-white' }} 
     flex items-center px-3 py-2 text-sm font-medium rounded-md"
     aria-current="{{ $active ? 'page' : false }}">
     <span>{{ $slot }}</span>
