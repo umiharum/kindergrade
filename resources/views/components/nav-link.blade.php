835af792ef2686b5580c1129265bbbca5870d876
umiharum <umiharum539@gmail.com>
2024-07-23 12:11:22 +0700

    navbar changes

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,6 @@
 <a {{ $attributes }}
     class="{{ $active ? 'text-gray-800 bg-gray-300' : 'text-gray-800 hover:bg-gray-700 hover:text-white' }} 
-    flex items-center px-3 py-2 text-sm font-medium rounded-md"
+    flex items-center px-3 py-2 text-sm font-medium rounded-md w-full"
     aria-current="{{ $active ? 'page' : false }}">
     <span>{{ $slot }}</span>
 </a>
